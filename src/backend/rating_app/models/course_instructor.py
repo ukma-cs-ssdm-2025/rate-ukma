41ceb26dbb8158edc46cf570e0a68f81145e3edd
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-10-13 18:54:20 +0300

    feat(#86): rename InstructorAssignment to CourseInstructor

---
+++ src/backend/rating_app/models/course_instructor.py
@@ -0,0 +1,26 @@
+import uuid
+
+from choices import InstructorRole
+from django.db import models
+
+
+class CourseInstructor(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    instructor = models.ForeignKey(
+        "rating_app.Instructor", on_delete=models.CASCADE, related_name="assignments"
+    )
+    course_offering = models.ForeignKey(
+        "rating_app.CourseOffering",
+        on_delete=models.CASCADE,
+        related_name="instructor_assignments",
+    )
+    role = models.TextChoices("InstructorRole", choices=InstructorRole)
+
+    class Meta:
+        unique_together = ("instructor", "course_offering", "role")
+
+    def __str__(self):
+        return f"{self.instructor} - {self.course_offering} ({self.role})"
+
+    def __repr__(self):
+        return f"{self.instructor} - {self.course_offering} ({self.role})"
