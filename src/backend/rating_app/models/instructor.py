3c9a9a12194650b7e878ddf95222c4e57832281c
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-10-13 17:59:58 +0300

    feat(#86): add __str__, __repr__ to Instructor

--- src/backend/rating_app/models/instructor.py
+++ src/backend/rating_app/models/instructor.py
@@ -13,3 +13,9 @@ class Instructor(Person):
     class Meta(Person.Meta):
         abstract = False
         managed = False
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}. ({self.academic_degree}, {self.academic_title})"
+
+    def __repr__(self) -> str:
+        return f"<Instructor id={self.id} last_name={self.last_name} first_name={self.first_name} degree={self.academic_degree} title={self.academic_title}>"
