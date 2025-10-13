9eade3e5aae82ad5fa655c098c77a38a67b36e24
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-10-14 00:03:47 +0300

    feat(#86): update CourseAdmin with correct fields

--- src/backend/rating_app/admin/base.py
+++ src/backend/rating_app/admin/base.py
@@ -4,8 +4,16 @@ from rating_app.models import Course
 
 @admin.register(Course)
 class CourseAdmin(VersionAdmin):
-    list_display = ("code", "title", "faculty", "department", "status", "type_kind")
-    list_select_related = ("faculty", "department")
-    list_filter = ("faculty", "department", "status", "type_kind")
-    search_fields = ("code", "title", "faculty__name", "department__name")
-    ordering = ("title",) 
+    list_display = (
+        "id",
+        "title",
+        "department",
+        "status",
+        "avg_difficulty",
+        "avg_usefulness",
+        "ratings_count",
+    )
+    list_select_related = ("department",)
+    list_filter = ("department", "status")
+    search_fields = ("title", "department__name")
+    ordering = ("title",)
