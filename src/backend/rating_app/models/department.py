d678fd5d158dbbacd1e71d8dffffbc28113ce94a
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-10-13 22:09:38 +0300

    style(#86): format code

--- src/backend/rating_app/models/department.py
+++ src/backend/rating_app/models/department.py
@@ -1,19 +1,21 @@
 from django.db import models
 import uuid
 
+
 class Department(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    faculty = models.ForeignKey('Faculty', on_delete=models.CASCADE, related_name='departments')
+    faculty = models.ForeignKey(
+        "Faculty", on_delete=models.CASCADE, related_name="departments"
+    )
 
-    def __str__(self): 
+    def __str__(self):
         return self.name
+    
     def __repr__(self): 
         return f"<Department name={self.name}> faculty_id={self.faculty.id}> faculty_name={self.faculty.name}>"
-
 
     class Meta:
         verbose_name = "Department"
         verbose_name_plural = "Departments"
         managed = False
-
--- src/backend/rating_app/models/speciality.py
+++ src/backend/rating_app/models/speciality.py
@@ -5,11 +5,10 @@ import uuid
 class Speciality(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
-    faculty = models.ForeignKey('Faculty', on_delete=models.CASCADE, related_name='specialities')
+    faculty = models.ForeignKey("Faculty", on_delete=models.CASCADE, related_name='specialities')
 
     class Meta:
         managed = False
 
     def __str__(self):
         return self.name
-
