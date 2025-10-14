47d5b0bf4aab0ede4ebd77b52001bbf9cb82bc85
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2025-10-14 12:12:03 +0300

    fix(#65): correct paths, override lists to have infinite query

--- src/webapp/orval.config.ts
+++ src/webapp/orval.config.ts
@@ -1,34 +1,36 @@
-import { defineConfig } from 'orval';
+import { defineConfig } from "orval";
 
 export default defineConfig({
-  api: {
-    output: {
-      mode: 'split',
-      target: 'ts-swc',
-      client: 'react-query',
-      override: {
-        operations: {
-          courses_list: {
-            query: {
-              useInfinite: true,
-            },
-          },
-          course_ratings_list: {
-            query: {
-              useInfinite: true,
-            },
-          },
-        },
-      },
-      schemas: 'src/lib/api-generated/models',
-      mock: false,
-      workspace: 'src/lib/api-generated',
-    },
-    input: {
-      target: '../../docs/api/openapi-generated.yaml',
-    },
-    hooks: {
-      afterAllFilesWrite: 'biome format --write src/lib/api-generated/**/*.{ts,tsx}',
-    },
-  },
+	api: {
+		output: {
+			mode: "split",
+			target: "src/lib/api-generated/index.ts",
+			workspace: "src/lib/api-generated",
+			schemas: "src/lib/api-generated/models",
+			client: "react-query",
+			override: {
+				operations: {
+					courses_list: {
+						query: {
+							useInfinite: true,
+						},
+					},
+					course_ratings_list: {
+						query: {
+							useInfinite: true,
+						},
+					},
+				},
+			},
+			// Do not generate MSW mocks
+			mock: false,
+		},
+		input: {
+			target: "../../docs/api/openapi-generated.yaml",
+		},
+		hooks: {
+			afterAllFilesWrite:
+				"biome format --write src/lib/api-generated/**/*.{ts,tsx}",
+		},
+	},
 });
