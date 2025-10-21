fab2ca84f8271031a901e12b0b13175e346f331a
stasiaaleks <alexeenko049@gmail.com>
2025-10-21 11:35:02 +0300

    feat(#131): add verbose names to specialities, add faculty creation in injector

--- src/backend/scraper/services/db_ingestion/injector.py
+++ src/backend/scraper/services/db_ingestion/injector.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel
 
 from rateukma.protocols.decorators import implements
 from rateukma.protocols.generic import IOperation
-from rating_app.models import Course, Department, Speciality
+from rating_app.models import Course, Department, Faculty, Speciality
 
 from ...models.deduplicated import DeduplicatedCourse, EducationLevel
 
@@ -30,13 +30,14 @@ class CourseDbInjector(IDbInjector):
     @implements
     def execute(self, models: Sequence[DeduplicatedCourse]) -> None:
         for course_data in models:
-            department = self._get_or_create_department(course_data.department, course_data.faculty)
+            faculty = self._get_or_create_faculty(course_data.faculty)
+            department = self._get_or_create_department(course_data.department, faculty)
 
             course = self._get_or_create_course(course_data.title, department)
             for spec_data in course_data.specialities:
                 speciality = self._get_or_create_speciality(
                     spec_data.name,
-                    spec_data.faculty,
+                    faculty,
                     spec_data.type.value if spec_data.type else EducationLevel.BACHELOR.value,
                 )
                 course.specialities.add(speciality)  # type: ignore
@@ -49,15 +50,19 @@ class CourseDbInjector(IDbInjector):
                 specialities_count=len(course_data.specialities),
             )
 
-    def _get_or_create_department(self, department_name: str, faculty: str) -> Department:
+    def _get_or_create_faculty(self, faculty_name: str) -> Faculty:
+        faculty, _ = Faculty.objects.get_or_create(name=faculty_name)  # type: ignore
+        return faculty
+
+    def _get_or_create_department(self, department_name: str, faculty: Faculty) -> Department:
         department, _ = Department.objects.get_or_create(name=department_name, faculty=faculty)  # type: ignore
         return department
 
     def _get_or_create_speciality(
-        self, speciality_name: str, faculty: str, education_level: str
+        self, speciality_name: str, faculty: Faculty, education_level: str
     ) -> Speciality:
         speciality, _ = Speciality.objects.get_or_create(  # type: ignore
-            name=speciality_name, faculty=faculty, defaults={"education_level": education_level}
+            name=speciality_name, faculty=faculty
         )
         return speciality
 
