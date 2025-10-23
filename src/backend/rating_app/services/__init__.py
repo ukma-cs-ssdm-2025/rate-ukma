250744065edd34be2908dc5b8503537d09a0ece4
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-10-23 19:54:50 +0300

    feat(#163): add Instructor service to ioc_container

--- src/backend/rating_app/services/__init__.py
+++ src/backend/rating_app/services/__init__.py
@@ -1,7 +1,9 @@
 from .course_service import CourseService
+from .instructor_service import InstructorService
 from .rating_service import RatingService
 
 __all__ = [
     "CourseService",
     "RatingService",
+    "InstructorService",
 ]
