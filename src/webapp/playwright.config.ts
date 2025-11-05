ed31a3c8b959acb3460c34e8a40d0bfe30a5fed1
Anastasiia Aleksieienko <103525721+stasiaaleks@users.noreply.github.com>
2025-11-05 10:53:53 +0200

    test(#136): succesful login and courses page e2e tests
    * test(#136): setup and create login e2e tests with playwright
    
    * ci(#136): run e2e tests after deployment
    
    * ci(#136): use package.json node version
    
    * test(#136): use requestPromise in login test
    
    * test(#136): add  e2e test with successful login
    
    * chore(ci): update coverage badge
    
    * test(#136): add login tests env vars to ci workflow
    
    * test(#136): configure required authentication before running tests, create initial course page tests
    
    * fix(#136): remove redundant stay signed in check
    
    * security(#136): improve regexps
    
    * chore(#136): add playwright/.auth to gitignore
    
    ---------
    
    Co-authored-by: stasiaaleks <anastasiia@newtonideas.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/webapp/playwright.config.ts
+++ src/webapp/playwright.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig, devices } from "@playwright/test";
 
+const authenticatedProject = {
+	dependencies: ["login"],
+	testIgnore: "**/login.test.ts",
+	use: {
+		storageState: "playwright/.auth/microsoft.json",
+	},
+};
+
 export default defineConfig({
 	testDir: "./tests/e2e",
 	use: {
@@ -8,9 +16,26 @@ export default defineConfig({
 		screenshot: "only-on-failure",
 	},
 	projects: [
-		{ name: "chromium", use: { ...devices["Desktop Chrome"] } },
-		{ name: "firefox", use: { ...devices["Desktop Firefox"] } },
-		{ name: "webkit", use: { ...devices["Desktop Safari"] } },
+		{
+			name: "login",
+			testMatch: "**/login.test.ts",
+			use: { ...devices["Desktop Chrome"] },
+		},
+		{
+			name: "chromium-auth",
+			...authenticatedProject,
+			use: { ...authenticatedProject.use, ...devices["Desktop Chrome"] },
+		},
+		{
+			name: "firefox-auth",
+			...authenticatedProject,
+			use: { ...authenticatedProject.use, ...devices["Desktop Firefox"] },
+		},
+		{
+			name: "webkit-auth",
+			...authenticatedProject,
+			use: { ...authenticatedProject.use, ...devices["Desktop Safari"] },
+		},
 	],
 	reporter: process.env.CI === "true" ? "html" : "line",
 });
--- src/webapp/tests/e2e/login.test.ts
+++ src/webapp/tests/e2e/login.test.ts
@@ -3,9 +3,14 @@ import { expect, test } from "@playwright/test";
 const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
 const MICROSOFT_LOGIN_REDIRECT_URL = "/api/v1/auth/login/microsoft/";
 const MICROSOFT_LOGIN_PAGE_PATTERN = /.*login.microsoftonline.com.*/;
+
 const MICROSOFT_LOGIN_BUTTON_TEXT = "Увійти";
 const MICROSOFT_LOGIN_TITLE_TEXT = "Вхід";
+const COURSES_PAGE_TITLE_TEXT = "Курси";
 const RATE_UKMA_TITLE_PATTERN = /Rate UKMA/;
+
+const MICROSOFT_EMAIL = process.env.CORPORATE_EMAIL ?? "";
+const MICROSOFT_PASSWORD = process.env.CORPORATE_PASSWORD ?? "";
 
 test.describe("Microsoft Login Page", () => {
 	test.beforeEach(async ({ page }) => {
@@ -52,5 +57,35 @@ test.describe("Microsoft Login Page", () => {
 
 		const request = await requestPromise;
 		expect(request.url()).toContain(MICROSOFT_LOGIN_REDIRECT_URL);
+	});
+
+	test("login with Microsoft account", async ({ page, context }) => {
+		if (!MICROSOFT_EMAIL || !MICROSOFT_PASSWORD) {
+			throw new Error("MICROSOFT_EMAIL and MICROSOFT_PASSWORD must be set");
+		}
+
+		const microsoftButton = page.locator("button", {
+			hasText: MICROSOFT_LOGIN_BUTTON_TEXT,
+		});
+		await microsoftButton.click();
+
+		// input email
+		await page.waitForURL(MICROSOFT_LOGIN_PAGE_PATTERN, { timeout: 10000 });
+		await page.fill('input[type="email"]', MICROSOFT_EMAIL);
+		await page.getByRole("button", { name: "Next" }).click();
+
+		// input password
+		await page.waitForSelector('input[type="password"]', { timeout: 10000 });
+		await page.fill('input[type="password"]', MICROSOFT_PASSWORD);
+		await page.getByRole("button", { name: "Sign in" }).click();
+
+		await page.waitForURL(BASE_URL, { timeout: 20000 });
+
+		// on the courses page
+		await expect(
+			page.locator("h1").filter({ hasText: COURSES_PAGE_TITLE_TEXT }),
+		).toBeVisible();
+
+		await context.storageState({ path: "playwright/.auth/microsoft.json" });
 	});
 });
