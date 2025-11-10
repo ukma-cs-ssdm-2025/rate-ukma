e17ef1ef5f2e2b405077d5a99b18b59c8d802754
stasiaaleks <anastasiia@newtonideas.com>
2025-11-10 23:24:35 +0200

    fix(#229): return QuerySet instead of list

--- src/backend/rating_app/repositories/protocol.py
+++ src/backend/rating_app/repositories/protocol.py
@@ -1,16 +1,16 @@
 from typing import Any, Protocol, TypeVar
 
-from django.db.models import Model
+from django.db.models import Model, QuerySet
 
 T = TypeVar("T", bound=Model)
 
 
 class IRepository(Protocol[T]):
-    def get_all(self) -> list[T]: ...
+    def get_all(self) -> QuerySet[T]: ...
 
     def get_by_id(self, id: str) -> T: ...
 
-    def filter(self, *args: Any, **kwargs: Any) -> list[T]: ...
+    def filter(self, *args: Any, **kwargs: Any) -> QuerySet[T]: ...
 
     def get_or_create(self, **kwargs: Any) -> tuple[T, bool]: ...
 
