ae289eb8bd7fcaae8221a92088c2163fcfe9859b
Anastasiia Aleksieienko <alexeenko049@gmail.com>
2025-12-04 12:29:45 +0200

    chore(#359): add caching to my ratings

--- src/backend/rating_app/views/student_viewset.py
+++ src/backend/rating_app/views/student_viewset.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 
 from drf_spectacular.utils import extend_schema
 
+from rateukma.caching.decorators import rcached
 from rating_app.models import Student
 from rating_app.serializers import StudentRatingsDetailedSerializer, StudentRatingsLightSerializer
 from rating_app.services import StudentService
@@ -22,7 +23,8 @@ class StudentStatisticsViewSet(viewsets.ViewSet):
         responses=R_STUDENT_RATINGS,
     )
     @require_student
-    def get_ratings(self, request, student: Student):
+    @rcached(ttl=300)
+    def get_ratings(self, request, student: Student) -> Response:
         assert self.student_service is not None
 
         items = self.student_service.get_ratings(student_id=str(student.id))
@@ -36,8 +38,9 @@ class StudentStatisticsViewSet(viewsets.ViewSet):
         "student is/was enrolled in with information about the rating.",
         responses=R_STUDENT_RATINGS_DETAILED,
     )
+    @rcached(ttl=300)
     @require_student
-    def get_detailed_ratings(self, request, student: Student):
+    def get_detailed_ratings(self, request, student: Student) -> Response:
         assert self.student_service is not None
 
         items = self.student_service.get_ratings_detail(student_id=str(student.id))
