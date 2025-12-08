be65f004a2ff564c4855d934499f969c4c9ea38e
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2025-12-09 01:40:53 +0200

    feat: remove UserProfileMiddleware
    - added overhead

--- src/backend/rateukma/settings/_base.py
+++ src/backend/rateukma/settings/_base.py
@@ -79,7 +79,6 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "rating_app.middleware.UserProfileMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
