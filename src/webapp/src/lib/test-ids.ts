106e60140c529cfc26bc66e4aa4b52027d09dd29
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2025-12-14 01:13:44 +0200

    feat: add footer /w link to github

--- src/webapp/src/lib/test-ids.ts
+++ src/webapp/src/lib/test-ids.ts
@@ -92,6 +92,11 @@ export const testIds = {
 		logoutButton: "header-logout-button",
 	},
 
+	// Footer
+	footer: {
+		repoLink: "footer-repo-link",
+	},
+
 	// Courses page
 	courses: {
 		searchInput: "courses-search-input",
