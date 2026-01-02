72e887f28fc74d1e20af770ad6d901302b77ff92
Anastasiia Aleksieienko <103525721+stasiaaleks@users.noreply.github.com>
2026-01-02 01:18:53 +0100

    fix(#380): cache drf headers (#460)
    * fix(#380): preserve drf headers and status code in cache, update tests
    
    * fix(#380): add a TODO for future improvement
    
    * fix(#380): prevent keys collision, add backward compatibility

--- src/backend/rateukma/caching/types_extensions.py
+++ src/backend/rateukma/caching/types_extensions.py
@@ -90,6 +90,7 @@ class TypeAdapterCacheExtension(ICacheTypeExtension[Any]):
         return TypeAdapter(value_type)
 
 
+# TODO: prevent caching non-2xx responses
 class DRFResponseCacheTypeExtension(ICacheTypeExtension[Response]):
     def __init__(self, cache_key_provider: CacheKeyContextProvider):
         self._cache_key_provider = cache_key_provider
@@ -127,19 +128,41 @@ class DRFResponseCacheTypeExtension(ICacheTypeExtension[Response]):
             return f"{func_name}:{path_with_query}"
 
     def serialize(self, value: Response, value_type: type[Response]) -> JSON_Serializable:
-        if isinstance(value.data, dict):
-            return {"_wrapped": False, **value.data}
-        return {"_wrapped": True, "data": value.data}
+        reserved_keys = {"_wrapped", "status_code", "headers"}
+
+        if isinstance(value.data, dict) and not reserved_keys.intersection(value.data.keys()):
+            #  no reserved keys are present
+            return {
+                "_wrapped": False,
+                **value.data,
+                "status_code": value.status_code,
+                "headers": dict(value.headers),
+            }
+
+        return {
+            "_wrapped": True,
+            "data": value.data,
+            "status_code": value.status_code,
+            "headers": dict(value.headers),
+        }
 
     def deserialize(self, data: JSON_Serializable, value_type: type[Response]) -> Response:
         if not isinstance(data, dict):
             raise TypeError(f"Expected dict for Response deserialization, got {type(data)}")
 
-        if data.get("_wrapped"):
-            return Response(data=data["data"])
+        response_headers = data.get("headers", {})
+        response_status = data.get("status_code", 200)
+        is_wrapped = data.get("_wrapped")
 
-        data.pop("_wrapped", None)
-        return Response(data=data)
+        if is_wrapped:
+            return Response(data=data["data"], status=response_status, headers=response_headers)
+
+        payload = {
+            key: value
+            for key, value in data.items()
+            if key not in {"_wrapped", "status_code", "headers"}
+        }
+        return Response(data=payload, status=response_status, headers=response_headers)
 
 
 class CacheTypeExtensionRegistry:
